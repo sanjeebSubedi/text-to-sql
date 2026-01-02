92bee9b2da60a6e6ddef0b50344887bac473f0c8
Sanjeeb Subedi <sanjeebsubedi4@gmail.com>
2026-01-01 21:27:48 -0600

    initial commit

---
+++ src/data/prompts/__init__.py
@@ -0,0 +1,16 @@
+# Prompt templates package
+from .templates import (
+    PROMPT_VERSION,
+    SYSTEM_PROMPT,
+    Qwen25Template,
+    Phi3Template,
+    get_template,
+)
+
+__all__ = [
+    "PROMPT_VERSION",
+    "SYSTEM_PROMPT",
+    "Qwen25Template",
+    "Phi3Template",
+    "get_template",
+]
